0032938f95a59d988c2047dbdfcf58cf485ff802
Isa Longoria <isalv@isas-air.devices.brown.edu>
2022-02-18 12:10:03 -0500

    changes

--- generator_model.py
+++ generator_model.py
@@ -63,7 +63,7 @@ z = torch.randn(8, 100, 1, 1, device=device)
 fake_image = netG(z)
 print(fake_image.shape)
 
-plt.imshow(fake_image[0].squeeze().detach().cpu().numpy())
+plt.imshow(fake_image[5].squeeze().detach().cpu().numpy())
 #plt.show()
 plt.savefig('test.png')
 
