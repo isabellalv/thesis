9feac7f4097c1632d1e81a62f250cf88512c3cea
Isa Longoria <isalv@isas-air.devices.brown.edu>
2022-03-06 17:49:47 -0500

    adding conditional GAN

---
+++ cifar_cDCGAN.py
@@ -0,0 +1,442 @@
+# MNIST image generation using Conditional DCGAN --> trying to adapt to CIFAR-10 using the previous DCGAN framework 
+import torch
+from torch.autograd import Variable
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import imageio
+from logger import Logger
+from draw_convnet import DrawNet
+
+# Parameters
+image_size = 32 
+label_dim = 10 #for each of the classes in the set, CIFAR has 10 
+G_input_dim = 100
+G_output_dim = 3
+D_input_dim = 3
+D_output_dim = 1
+num_filters = [512, 256, 128]
+
+learning_rate = 0.0002
+betas = (0.5, 0.999)
+batch_size = 128 
+num_epochs = 50 
+data_dir = '../../Data/CIFAR_data/' #maybe this is where I need to line it up for CIFAR, since this is where it's getting the code? 
+save_dir = 'CIFAR_cDCGAN_results/' 
+
+# loading the CIFAR dataset 
+transform = transforms.Compose([transforms.Scale(image_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+#getting dataset --> how does it know where this dataset is coming from??? Do I have to have CIFAR somewhere? 
+cifar_data = dsets.CIFAR10(root=data_dir,
+                         train=True,
+                         transform=transform,
+                         download=True)
+
+data_loader = torch.utils.data.DataLoader(dataset=cifar_data,
+                                          batch_size=batch_size,
+                                          shuffle=True)
+
+#do you want to specify what device to run it on? 
+
+###################
+##IMPLEMENTATION: 
+###################
+
+# For logger - is this the weights initialization?
+def to_np(x):
+    return x.data.cpu().numpy()
+
+
+def to_var(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+
+
+# De-normalization
+def denorm(x):
+    out = (x + 1) / 2
+    return out.clamp(0, 1)
+
+
+# Generator model
+class Generator(torch.nn.Module):
+    def __init__(self, input_dim, label_dim, num_filters, output_dim):
+        super(Generator, self).__init__()
+
+        # Hidden layers
+        self.hidden_layer1 = torch.nn.Sequential()
+        self.hidden_layer2 = torch.nn.Sequential()
+        self.hidden_layer = torch.nn.Sequential()
+        for i in range(len(num_filters)):
+            # Deconvolutional layer
+            if i == 0:
+                # For input image in CIFAR
+                input_deconv = torch.nn.ConvTranspose2d(input_dim, int(num_filters[i]/2), kernel_size=4, stride=1, padding=0)
+                self.hidden_layer1.add_module('input_deconv', input_deconv)
+
+                # Initializer - with weight biases 
+                torch.nn.init.normal(input_deconv.weight, mean=0.0, std=0.02)
+                torch.nn.init.constant(input_deconv.bias, 0.0)
+
+                # Batch normalization - for speed and stability 
+                self.hidden_layer1.add_module('input_bn', torch.nn.BatchNorm2d(int(num_filters[i]/2)))
+
+                # Activation function to learn non-linear relationship 
+                self.hidden_layer1.add_module('input_act', torch.nn.ReLU())
+
+                # For class label in CIFAR
+                label_deconv = torch.nn.ConvTranspose2d(label_dim, int(num_filters[i]/2), kernel_size=4, stride=1, padding=0)
+                self.hidden_layer2.add_module('label_deconv', label_deconv)
+
+                # Initializer
+                torch.nn.init.normal(label_deconv.weight, mean=0.0, std=0.02)
+                torch.nn.init.constant(label_deconv.bias, 0.0)
+
+                # Batch normalization
+                self.hidden_layer2.add_module('label_bn', torch.nn.BatchNorm2d(int(num_filters[i]/2)))
+
+                # Activation
+                self.hidden_layer2.add_module('label_act', torch.nn.ReLU())
+            else:
+                deconv = torch.nn.ConvTranspose2d(num_filters[i-1], num_filters[i], kernel_size=4, stride=2, padding=1)
+
+                deconv_name = 'deconv' + str(i + 1)
+                self.hidden_layer.add_module(deconv_name, deconv)
+
+                # Initializer
+                torch.nn.init.normal(deconv.weight, mean=0.0, std=0.02)
+                torch.nn.init.constant(deconv.bias, 0.0)
+
+                # Batch normalization
+                bn_name = 'bn' + str(i + 1)
+                self.hidden_layer.add_module(bn_name, torch.nn.BatchNorm2d(num_filters[i]))
+
+                # Activation
+                act_name = 'act' + str(i + 1)
+                self.hidden_layer.add_module(act_name, torch.nn.ReLU())
+
+        # Output layer of Generator 
+        # could add a couple lines of code here to make network able to handle multiple gpus
+        self.output_layer = torch.nn.Sequential()
+        # Deconvolutional layer
+        out = torch.nn.ConvTranspose2d(num_filters[i], output_dim, kernel_size=4, stride=2, padding=1)
+        self.output_layer.add_module('out', out)
+        # Initializer
+        torch.nn.init.normal(out.weight, mean=0.0, std=0.02)
+        torch.nn.init.constant(out.bias, 0.0)
+        # Activation
+        self.output_layer.add_module('act', torch.nn.Tanh())
+
+    def forward(self, z, c):
+        h1 = self.hidden_layer1(z)
+        h2 = self.hidden_layer2(c)
+        x = torch.cat([h1, h2], 1)
+        h = self.hidden_layer(x)
+        out = self.output_layer(h)
+        return out
+
+
+# Discriminator model
+class Discriminator(torch.nn.Module):
+    def __init__(self, input_dim, label_dim, num_filters, output_dim):
+        super(Discriminator, self).__init__()
+
+        self.hidden_layer1 = torch.nn.Sequential()
+        self.hidden_layer2 = torch.nn.Sequential()
+        self.hidden_layer = torch.nn.Sequential()
+        for i in range(len(num_filters)):
+            # Convolutional layer
+            if i == 0:
+                # For input in CIFAR
+                input_conv = torch.nn.Conv2d(input_dim, int(num_filters[i]/2), kernel_size=4, stride=2, padding=1)
+                self.hidden_layer1.add_module('input_conv', input_conv)
+
+                # Initializer
+                torch.nn.init.normal(input_conv.weight, mean=0.0, std=0.02)
+                torch.nn.init.constant(input_conv.bias, 0.0)
+
+                # Activation
+                self.hidden_layer1.add_module('input_act', torch.nn.LeakyReLU(0.2))
+
+                # For class label in CIFAR
+                label_conv = torch.nn.Conv2d(label_dim, int(num_filters[i]/2), kernel_size=4, stride=2, padding=1)
+                self.hidden_layer2.add_module('label_conv', label_conv)
+
+                # Initializer
+                torch.nn.init.normal(label_conv.weight, mean=0.0, std=0.02)
+                torch.nn.init.constant(label_conv.bias, 0.0)
+
+                # Activation
+                self.hidden_layer2.add_module('label_act', torch.nn.LeakyReLU(0.2))
+            else:
+                conv = torch.nn.Conv2d(num_filters[i-1], num_filters[i], kernel_size=4, stride=2, padding=1)
+
+                conv_name = 'conv' + str(i + 1)
+                self.hidden_layer.add_module(conv_name, conv)
+
+                # Initializer
+                torch.nn.init.normal(conv.weight, mean=0.0, std=0.02)
+                torch.nn.init.constant(conv.bias, 0.0)
+
+                # Batch normalization
+                bn_name = 'bn' + str(i + 1)
+                self.hidden_layer.add_module(bn_name, torch.nn.BatchNorm2d(num_filters[i]))
+
+                # Activation
+                act_name = 'act' + str(i + 1)
+                self.hidden_layer.add_module(act_name, torch.nn.LeakyReLU(0.2))
+
+        # Output layer
+        self.output_layer = torch.nn.Sequential()
+        # Convolutional layer
+        out = torch.nn.Conv2d(num_filters[i], output_dim, kernel_size=4, stride=1, padding=0)
+        self.output_layer.add_module('out', out)
+        # Initializer
+        torch.nn.init.normal(out.weight, mean=0.0, std=0.02)
+        torch.nn.init.constant(out.bias, 0.0)
+        # Activation
+        self.output_layer.add_module('act', torch.nn.Sigmoid()) #this is the equivalent of the load_state_dict in previous model
+
+    def forward(self, z, c):
+        h1 = self.hidden_layer1(z)
+        h2 = self.hidden_layer2(c)
+        x = torch.cat([h1, h2], 1)
+        h = self.hidden_layer(x)
+        out = self.output_layer(h)
+        return out
+
+################
+#PLOTTING:
+################
+
+# Plot losses - other model uses BCELoss function 
+def plot_loss(d_losses, g_losses, num_epoch, save=False, save_dir='CIFAR_cDCGAN_results/', show=False):
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, num_epochs)
+    ax.set_ylim(0, max(np.max(g_losses), np.max(d_losses))*1.1)
+    plt.xlabel('Epoch {0}'.format(num_epoch + 1))
+    plt.ylabel('Loss values')
+    plt.plot(d_losses, label='Discriminator')
+    plt.plot(g_losses, label='Generator')
+    plt.legend()
+
+    # save figure
+    if save:
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        save_fn = save_dir + 'CIFAR_cDCGAN_losses_epoch_{:d}'.format(num_epoch + 1) + '.png'
+        plt.savefig(save_fn)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+#plotting results 
+
+def plot_result(generator, noise, label, num_epoch, save=False, save_dir='CIFAR_cDCGAN_results/', show=False, fig_size=(5, 5)):
+    generator.eval()
+
+    noise = Variable(noise.cuda())
+    label = Variable(label.cuda())
+    gen_image = generator(noise, label)
+    gen_image = denorm(gen_image)
+
+    generator.train()
+
+    n_rows = np.sqrt(noise.size()[0]).astype(np.int32)
+    n_cols = np.sqrt(noise.size()[0]).astype(np.int32)
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=fig_size)
+    for ax, img in zip(axes.flatten(), gen_image):
+        ax.axis('off')
+        ax.set_adjustable('box-forced')
+        ax.imshow(img.cpu().data.view(image_size, image_size).numpy(), cmap='gray', aspect='equal')
+    plt.subplots_adjust(wspace=0, hspace=0)
+    title = 'Epoch {0}'.format(num_epoch+1)
+    fig.text(0.5, 0.04, title, ha='center')
+
+    # save figure
+    if save:
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        save_fn = save_dir + 'CIFAR_cDCGAN_epoch_{:d}'.format(num_epoch+1) + '.png'
+        plt.savefig(save_fn)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+# Models
+G = Generator(G_input_dim, label_dim, num_filters, G_output_dim)
+D = Discriminator(D_input_dim, label_dim, num_filters[::-1], D_output_dim)
+G.cuda()
+D.cuda()
+
+# Draw net
+G_net = DrawNet([1, 1], G_input_dim, num_filters, [image_size, image_size], G_output_dim, 4, 'Deconv2D', save_dir)
+D_net = DrawNet([image_size, image_size], D_input_dim, num_filters[::-1], [1, 1], D_output_dim, 4, 'Conv2D', save_dir)
+G_net.draw()
+D_net.draw()
+
+# Set the logger
+D_log_dir = save_dir + 'D_logs'
+G_log_dir = save_dir + 'G_logs'
+if not os.path.exists(D_log_dir):
+    os.mkdir(D_log_dir)
+D_logger = Logger(D_log_dir)
+
+if not os.path.exists(G_log_dir):
+    os.mkdir(G_log_dir)
+G_logger = Logger(G_log_dir)
+
+
+# Loss function
+criterion = torch.nn.BCELoss()
+
+# Optimizers
+G_optimizer = torch.optim.Adam(G.parameters(), lr=learning_rate, betas=betas)
+D_optimizer = torch.optim.Adam(D.parameters(), lr=learning_rate/2, betas=betas)
+
+################
+#TRAINING:
+################
+
+# Training GAN
+D_avg_losses = []
+G_avg_losses = []
+
+# Fixed noise & class label for test
+num_test_samples = 10*10
+
+temp_noise = torch.randn(label_dim, G_input_dim)
+fixed_noise = temp_noise
+fixed_c = torch.zeros(label_dim, 1)
+for i in range(9):
+    fixed_noise = torch.cat([fixed_noise, temp_noise], 0)
+    temp = torch.ones(label_dim, 1) + i
+    fixed_c = torch.cat([fixed_c, temp], 0)
+
+fixed_noise = fixed_noise.view(-1, G_input_dim, 1, 1)
+fixed_label = torch.zeros(G_input_dim, label_dim)
+fixed_label.scatter_(1, fixed_c.type(torch.LongTensor), 1)
+fixed_label = fixed_label.view(-1, label_dim, 1, 1)
+
+# label preprocess
+onehot = torch.zeros(label_dim, label_dim)
+onehot = onehot.scatter_(1, torch.LongTensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).view(label_dim, 1), 1).view(label_dim, label_dim, 1, 1)
+fill = torch.zeros([label_dim, label_dim, image_size, image_size])
+for i in range(label_dim):
+    fill[i, i, :, :] = 1
+
+#Training Loop 
+step = 0
+for epoch in range(num_epochs):
+    D_losses = []
+    G_losses = []
+
+    if epoch == 5 or epoch == 10:
+        G_optimizer.param_groups[0]['lr'] /= 2
+        D_optimizer.param_groups[0]['lr'] /= 2
+
+    # minibatch training
+    for i, (images, labels) in enumerate(data_loader):
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        # image data
+        mini_batch = images.size()[0]
+        x_ = Variable(images.cuda())
+
+        # labels
+        y_real_ = Variable(torch.ones(mini_batch).cuda())
+        y_fake_ = Variable(torch.zeros(mini_batch).cuda())
+        c_fill_ = Variable(fill[labels].cuda())
+
+        # Train discriminator with real data
+        D_real_decision = D(x_, c_fill_).squeeze()
+        D_real_loss = criterion(D_real_decision, y_real_)
+
+        # Train discriminator with fake data
+        z_ = torch.randn(mini_batch, G_input_dim).view(-1, G_input_dim, 1, 1)
+        z_ = Variable(z_.cuda())
+
+        c_ = (torch.rand(mini_batch, 1) * label_dim).type(torch.LongTensor).squeeze()
+        c_onehot_ = Variable(onehot[c_].cuda())
+        gen_image = G(z_, c_onehot_)
+
+        c_fill_ = Variable(fill[c_].cuda())
+        D_fake_decision = D(gen_image, c_fill_).squeeze()
+        D_fake_loss = criterion(D_fake_decision, y_fake_)
+
+        # Back propagation and calculating loss 
+        D_loss = D_real_loss + D_fake_loss
+        D.zero_grad()
+        D_loss.backward()
+        D_optimizer.step()
+
+        # Train generator
+        z_ = torch.randn(mini_batch, G_input_dim).view(-1, G_input_dim, 1, 1)
+        z_ = Variable(z_.cuda())
+
+        c_ = (torch.rand(mini_batch, 1) * label_dim).type(torch.LongTensor).squeeze()
+        c_onehot_ = Variable(onehot[c_].cuda())
+        gen_image = G(z_, c_onehot_)
+
+        c_fill_ = Variable(fill[c_].cuda())
+        D_fake_decision = D(gen_image, c_fill_).squeeze()
+        G_loss = criterion(D_fake_decision, y_real_)
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z))) (generate better fakes)
+        ###########################
+        # Back propagation
+        G.zero_grad()
+        G_loss.backward()
+        G_optimizer.step()
+
+        # loss values
+        D_losses.append(D_loss.data[0])
+        G_losses.append(G_loss.data[0])
+
+        print('Epoch [%d/%d], Step [%d/%d], D_loss: %.4f, G_loss: %.4f'
+              % (epoch+1, num_epochs, i+1, len(data_loader), D_loss.data[0], G_loss.data[0]))
+
+        # ============ TensorBoard logging: provides measurements/visualizations needed during workflow============#
+        D_logger.scalar_summary('losses', D_loss.data[0], step + 1)
+        G_logger.scalar_summary('losses', G_loss.data[0], step + 1)
+        step += 1
+
+    D_avg_loss = torch.mean(torch.FloatTensor(D_losses))
+    G_avg_loss = torch.mean(torch.FloatTensor(G_losses))
+
+    # avg loss values for plot
+    D_avg_losses.append(D_avg_loss)
+    G_avg_losses.append(G_avg_loss)
+
+    plot_loss(D_avg_losses, G_avg_losses, epoch, save=True)
+
+    # Show result for fixed noise
+    plot_result(G, fixed_noise, fixed_label, epoch, save=True)
+
+# Making a gif with class interpolation 
+loss_plots = []
+gen_image_plots = []
+for epoch in range(num_epochs):
+    # plot for generating gif
+    save_fn1 = save_dir + 'CIFAR_cDCGAN_losses_epoch_{:d}'.format(epoch + 1) + '.png'
+    loss_plots.append(imageio.imread(save_fn1))
+
+    save_fn2 = save_dir + 'CIFAR_cDCGAN_epoch_{:d}'.format(epoch + 1) + '.png'
+    gen_image_plots.append(imageio.imread(save_fn2))
+
+    ##doing checkingpointing: saves output for both D and G 
+
+imageio.mimsave(save_dir + 'CIFAR_cDCGAN_losses_epochs_{:d}'.format(num_epochs) + '.gif', loss_plots, fps=5)
+imageio.mimsave(save_dir + 'CIFAR_cDCGAN_epochs_{:d}'.format(num_epochs) + '.gif', gen_image_plots, fps=5)
