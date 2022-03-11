0771e204e396c0b0651446722b20b59435eddaf3
Isa Longoria <isalv@isas-air.devices.brown.edu>
2022-03-11 11:33:07 -0500

    compat v1

--- logger.py
+++ logger.py
@@ -17,8 +17,11 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.compat.v1.Summary(value=[tf.compat.v1.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        #summary = tf.compat.v1.Summary(value=[tf.compat.v1.Summary.Value(tag=tag, simple_value=value)])
+        #self.writer.add_summary(summary, step)
+
+        tf.summary.scalar(tag, value, step=step)
+        self.writer.flush()
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
