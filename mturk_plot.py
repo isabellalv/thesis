3493f1102260ce96aa256878ee24012e0000a899
Isa Longoria <isalv@cis6qvtmv2.ad.brown.edu>
2022-04-11 00:40:49 -0400

    adding plotting

---
+++ mturk_plot.py
@@ -0,0 +1,14 @@
+#PLOTTING DATA FROM MTURK CSV
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+mturk_data = pd.read_csv('mturk_results_b0_cleaned.csv')
+
+#mturk_data.Image_1
+
+plt.plot(mturk_data.Image_1, mturk_data.Similarity_Rating)
+plt.plot(mturk_data.Image_2, mturk_data.Similarity_Rating)
+
+
+plt.show()
