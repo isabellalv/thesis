d99e4fb2add65741071b1065c4c7169064cf944e
Isa Longoria <isalv@cis6qvtmv2.ad.brown.edu>
2022-04-19 16:20:23 -0400

    updating files

--- mturk_plot.py
+++ mturk_plot.py
@@ -3,11 +3,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import ipdb 
+ipdb.set_trace()
+
 mturk_data = pd.read_csv('mturk_results_b0_cleaned.csv')
 
 #mturk_data.Image_1
 
-plt.plot(mturk_data.Image_1, mturk_data.Similarity_Rating)
+
+
+#plt.plot(mturk_data.Image_1, mturk_data.Similarity_Rating)
 plt.plot(mturk_data.Image_2, mturk_data.Similarity_Rating)
 
 
